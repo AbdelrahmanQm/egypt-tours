2e147908072d0cdcf1dbc8139d1029be4d866a8f
Abdelrahman Qassem <abdelrahman.qassem@gmail.com>
2024-01-10 17:47:53 +0200

    Egypt Tours Init

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      yellow: "#faab36",
+      orange: "#f78104",
+      red: "#fd5901",
+      lightgreen: "#249ea0",
+      green: "#008083",
+      darkgreen: "#005f60",
+      gray: "#555",
+      darkgray: "#333",
+      white: "#ffffff",
+      lightgray: "#d3dce6",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
